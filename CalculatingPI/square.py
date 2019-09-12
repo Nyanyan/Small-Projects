39554d49dd619a77438dff0c9e1ce65a91b85542
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-12 19:35:28 +0900

    show status bar

--- CalculatingPI/square.py
+++ CalculatingPI/square.py
@@ -1,14 +1,27 @@
+import sys
 import random
 
-num = 1000
-r = 50
+num = 100000000
+r = 99/2
 count=0
+b = 50
+j=1
 for i in range(num):
     x = random.uniform(0,2*r)
     y = random.uniform(0,2*r)
-    print(x,y)
+    #print(x,y)
     if (x-r)**2 + (y-r)**2 <= r**2:
         count+=1
+    if i % (num // b) == 0:
+        sys.stdout.write("\r")
+        for k in range(j):
+            sys.stdout.write("=")
+        for k in range(b - j):
+            sys.stdout.write(" ")
+        sys.stdout.write("|")
+        sys.stdout.flush()
+        j+=1
 
 pi = 4 * count / num
+print('')
 print(count,num,pi)

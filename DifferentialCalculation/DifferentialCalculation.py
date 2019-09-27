122ea1fccd3dbac6b64a38607c5ca7ddca4ab2cc
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-27 22:11:45 +0900

    modified dt

--- DifferentialCalculation/DifferentialCalculation.py
+++ DifferentialCalculation/DifferentialCalculation.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
-dt = 0.001
+equation = "ma = -kx - k'(v^2)*(v/|v|)"
+
+dt = 0.00001
 x0 = 1
 v0 = 0
 a0 = -x0
 
-time = 10
+time = 100
 
 xarray = []
 varray = []
@@ -21,12 +23,12 @@ t = int(time // dt)
 for i in range(t):
     varray.append(varray[i] + aarray[i] * dt)
     xarray.append(xarray[i] + varray[i] * dt)
-    aarray.append(-xarray[i+1])
+    aarray.append(-(varray[i+1] ** 3) / abs(varray[i+1]) - xarray[i+1])
     tarray.append(tarray[i] + dt)
 
 plt.rcParams["font.family"] = "Times New Roman" # フォントの種類
-plt.plot(tarray, xarray, '-', label="data-with-error")
-#plt.title("fitting", fontsize=16) #題名
+plt.plot(tarray, xarray, '-', label="result")
+plt.title(equation, fontsize=16) #題名
 plt.xlabel("x", fontsize=12)     #x軸ラベル
 plt.ylabel("y", fontsize=12)    #y軸ラベル
 plt.grid() #グリッド表示

83afa92ba21555b37f82d74980230b3efc4d57f0
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-29 10:40:22 +0900

    TBD wrote 5

--- 1hChallenge/5.py
+++ 1hChallenge/5.py
@@ -1,6 +1,18 @@
+import math
+
 l = range(1,10)
 
-for i in range(len(l)):
-    for j in range(5):
-        if j == 0:
-            
+def a(i, num, n, plus):
+    out = 0
+    if n == 0:
+        num += l[i]
+    elif n == 1:
+        num -= l[i]
+    elif n == 2:
+        num *= l[i]
+    elif n == 3:
+        num /= l[i]
+    elif n == 4:
+        out = plus + l[i] ** (math.log10(plus) + 1)
+    return num, out
+

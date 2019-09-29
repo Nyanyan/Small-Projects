afacd5b579a68e0b0c5c0d6144b0ce065d95a410
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-09-29 11:53:40 +0900

    TBD

--- 1hChallenge/5.py
+++ 1hChallenge/5.py
@@ -8,7 +8,7 @@ def a(i, num, n, plus):
     out = 0
     tmp = 0
     if plus > 0:
-        tmp = plus + l[i] * 10 ** int(math.log10(plus) + 1)
+        tmp = plus * 10 + l[i]
     else:
         tmp = l[i]
     if n == 0:
@@ -20,7 +20,7 @@ def a(i, num, n, plus):
 def b(start, stop):
     out = 0
     for j in range(start, stop + 1):
-        out = a(stop-j, 0, 2, out)[1]
+        out = a(j, 0, 2, out)[1]
     return out
 
 
@@ -28,24 +28,40 @@ print(l)
 
 for i in range(len(l) - 2):#演算子の数
     n = list(itertools.combinations(cal,i)) #演算子の組み合わせ
-    for j in range(1): #一つの演算子配列 len(n)
+    for j in range(len(n)):
+        n[j] = list(n[j])
+        n[j].append(8)
+    for j in range(len(n)): #一つの演算子配列 len(n)
+        first = 0
         if len(n[j]) != 0: #最初の数字
-            num = b(0,n[j][0])
+            first = b(0,n[j][0])
         else:
-            num = b(0,8)
+            first = b(0,8)
+        num = [first]
+        pm = [[0]]
         #print(num)
         #print(n[j])
-        for k in range(len(n[j])): #一つの演算子
+        for k in range(len(n[j]) - 1): #一つの演算子
             #print(n[j][k])
             t = 0
             if k > 0:
                 t = n[j][k-1]
             plus = 0
-            for m in range(t, len(l) - 2):
-                if n[j][k] == m:
-                    num = a(m + 1, num, 0, plus)[0]
-                    print(plus)
-                else:
-                    plus = a(m + 1, num, 2, plus)[1]
-
-        print(n[j], num)
+            for m in range(t, len(l)):
+                if n[j][k+1] == m:
+                    tmp = len(num)
+                    for o in range(tmp):
+                        num.append(num[o])
+                        num[o] += plus
+                        num[tmp + o] -= plus
+                        pm.append(pm[o])
+                        pm[o].append('+')
+                        pm[tmp + o].append('-')
+                    break
+                elif n[j][k] <= m:
+                    plus = a(m + 1, 0, 0, plus)[1]
+                
+        for k in range(len(num)):
+            if num[k] == 100:
+                print(n[j],num[k], pm[k], k)
+        #print(n[j], num)
